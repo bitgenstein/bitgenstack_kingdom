92a51e92b59fb5f1365674d7b1b3745df4b2cec1
bitgenstein <peter@bitgenste.in>
2019-08-12 00:26:29 +0000

    init repo

---
+++ precache-manifest.e8e064e6414d92f0cbad6a43c21b8a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63704568ee19352c141601099f99aa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bacae53cfaccabd3f47",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "900dfbe4e72d08855abb",
+    "url": "/static/css/main.20034467.chunk.css"
+  },
+  {
+    "revision": "3bacae53cfaccabd3f47",
+    "url": "/static/js/2.4af7a3bb.chunk.js"
+  },
+  {
+    "revision": "900dfbe4e72d08855abb",
+    "url": "/static/js/main.d75f541b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
